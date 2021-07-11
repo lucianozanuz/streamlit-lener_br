beec06563f9b24fbbab1ced4fac4e46579a818f2
Luciano Zanuz <lzanuz@hotmail.com>
2021-07-11 08:48:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,10 @@ if uploaded_file is not None:
    bytes_data = uploaded_file.getvalue()
    st.write(bytes_data)
 
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
 #if st.sidebar.button('Enviar', key='bt_enviar'):
 #   st.sidebar.write('Why hello there')
 #else:
