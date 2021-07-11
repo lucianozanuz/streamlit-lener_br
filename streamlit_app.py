5ba05b6339d9827ce215a85506296a11f1064f6e
Luciano Zanuz <lzanuz@hotmail.com>
2021-07-11 08:04:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,18 @@ st.text('Carregue o arquivo de algum texto jurídico em PDF e clique em Enviar')
 #st.sidebar.subheader('Subheader da aplicação')
 #st.sidebar.text('Texto: Upload excel files with only one column, even if you put multiple columns only the first one will be used')
 
+def processa_pdf():
+    st.write('PDF processado')
+    st.write('PDF processado sidebar')
+
 file_match = st.sidebar.file_uploader("Selecione um PDF", help="Selecione um arquivo em PDF referente a uma petição ou texto jurídico.")
 if st.sidebar.button('Enviar'):
+   st.sidebar.write('Why hello there')
    st.sidebar.write('Why hello there')
 else:
    st.sidebar.write('Goodbye')
 
-if st.sidebar.button('Enviar cli', onclick='processa_pdf'):
+if st.sidebar.button('Enviar cli', on_click='processa_pdf'):
    st.sidebar.write('Why hello there')
 else:
    st.sidebar.write('Goodbye')
