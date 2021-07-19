6c7519f2f4bdbb3a69757ba78c930e207d1045e6
Luciano Zanuz <lzanuz@hotmail.com>
2021-07-19 07:57:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,19 +106,10 @@ def ner_pipeline(texto, modelo_treinado, tokenizer_treinado, aggregation_strateg
     ex = [{"text": texto,
           "ents": ents,
           "title": None}]
-    
-    
-    st.write(data)
-    st.write(ents)
-    #df1 = df = pd.DataFrame({"Entidade":[data.entity_group],
-    #                         "Valor":[data.word]})
-    
-    
-    
-    #df = pd.DataFrame(data)
+        
     df = pd.DataFrame(data,
                       columns=['entity_group', 'word'])
-    df.rename(columns = {'entity_group':'Entidade'}, inplace = True)
+    df.rename(columns = {'entity_group':'Entidade','word':'Valor'}, inplace = True)
     
     #df = pd.DataFrame(columns=['Entidade','Valor'])
     #for entity_group, word in item.items():
