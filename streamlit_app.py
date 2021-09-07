e6d2b3abcf27a8860dfb77d244e90834c6f82b92
lzanuz <lzanuz@hotmail.com>
2021-09-07 09:05:53 -0300

    Ajustando layout da aplicação para disponibilizar para usuários.

--- streamlit_app.py
+++ streamlit_app.py
@@ -242,10 +242,11 @@ elif (opt_pdf == "pdfminer por frase"):
             if (not item.isspace()):
                 ner_df, ner_displacy = ner_pipeline(item, modelo_treinado, tokenizer_treinado, aggregation_strategy)
                 st.write(ner_displacy, unsafe_allow_html=True)
+                st.table(ner_df)
                 #my_table = st.table(ner_df)
                 frames = [tbl, ner_df]
                 #tbl = pd.concat(frames)
-        st.table(tbl)
+        #st.table(tbl)
 
 elif (opt_pdf == "pdfplumber"):
     pdf_text = ""
