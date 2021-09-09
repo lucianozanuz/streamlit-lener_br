4fee529566113ad2813789f37f6ec947ba9552e3
lzanuz <lzanuz@hotmail.com>
2021-09-09 09:16:03 -0300

    Ajustando layout da aplicação para disponibilizar para usuários.

--- streamlit_app.py
+++ streamlit_app.py
@@ -297,7 +297,7 @@ elif opt_pdf == "pdfplumber por frase":
                 pdf_text += page.extract_text()
 
         #pdf_text.replace('\n', '')
-        pdf_text = pdf_text.replace('\n', '')
+        # pdf_text = pdf_text.replace('\n', '')
         if debug:
             st.write('pdf_text =', pdf_text)
 
