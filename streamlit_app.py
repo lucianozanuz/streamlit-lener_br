3d2626642c1459ec7be0893f8b1448f9186ed3c3
lzanuz <lzanuz@hotmail.com>
2021-09-10 07:11:49 -0300

    Ajustando layout da aplicação para disponibilizar para usuários.

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,7 +132,7 @@ def ner_pipeline(texto, modelo_treinado, tokenizer_treinado, aggregation_strateg
 # @st.cache(max_entries=5, ttl=300)
 # @st.cache(ttl=600, persist=True)
 # @st.cache(allow_output_mutation=True, ttl=3600)
-@st.cache(max_entries=2, ttl=86400)
+@st.cache(max_entries=1, ttl=86400)
 def carrega_modelo(modelo):
     st.write('Cache miss: carrega_modelo(',modelo,')')
     modelo_treinado = AutoModelForTokenClassification.from_pretrained(modelo)
@@ -140,6 +140,7 @@ def carrega_modelo(modelo):
 
 # @st.cache(allow_output_mutation=True, max_entries=5, ttl=300)  # Parâmetro necessário para não dar erro de hash
 # @st.cache(allow_output_mutation=True, ttl=3600)
+@st.cache()
 def carrega_tokenizer(modelo):
     st.write('Cache miss: carrega_tokenizer(',modelo,')')
     tokenizer_treinado = AutoTokenizer.from_pretrained(modelo)
