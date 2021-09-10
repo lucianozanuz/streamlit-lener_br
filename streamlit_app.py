b4448d289dabd4b9fab2acc3243d2cd7aa817ceb
lzanuz <lzanuz@hotmail.com>
2021-09-10 08:35:35 -0300

    Ajustando layout da aplicação para disponibilizar para usuários.

--- streamlit_app.py
+++ streamlit_app.py
@@ -143,7 +143,7 @@ def carrega_modelo(modelo):
 # @st.cache(allow_output_mutation=True, max_entries=5, ttl=300)  # Parâmetro necessário para não dar erro de hash
 # @st.cache(allow_output_mutation=True, ttl=3600)
 # @st.cache(suppress_st_warning=True, allow_output_mutation=True)
-@st.cache(suppress_st_warning=True, hash_funcs={AutoTokenizer: lambda _: None})
+@st.cache(suppress_st_warning=True, hash_funcs={BertTokenizerFast: lambda _: None})
 def carrega_tokenizer(modelo):
     st.write('Cache miss: carrega_tokenizer(',modelo,')')
     tokenizer_treinado = AutoTokenizer.from_pretrained(modelo)
