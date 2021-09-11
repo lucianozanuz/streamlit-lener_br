91b0872f144a23bb25909e066d2c3344699e3f38
lzanuz <lzanuz@hotmail.com>
2021-09-11 07:17:26 -0300

    Ajustando layout da aplicação para disponibilizar para usuários.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ st.title('Reconhecimento de Entidades Nomeadas Jurídicas')
 # st.header('Header da aplicação.')
 # st.subheader('This model is a fine-tuned version of neuralmind/bert-large-portuguese-cased on the lener_br dataset')
 st.text('Modelo de aprendizado profundo treinado a partir do BERTimbau utilizando o dataset LeNER-Br')
+st.write('A Web App by [Tyler Richards](http://www.tylerjrichards.com)')
 
 ### Parâmetros do processamento
 
