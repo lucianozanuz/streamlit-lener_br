41997f9bb2431962a4bdbc8b8404a7f75735befb
lzanuz <lzanuz@hotmail.com>
2021-10-07 07:09:45 -0300

    Adding carrega_spacy()

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,7 +160,7 @@ def ner_pipeline(texto, modelo_treinado, tokenizer_treinado, aggregation_strateg
 # @st.cache(suppress_st_warning=True)
 # @st.cache(suppress_st_warning=True, hash_funcs={AutoModelForTokenClassification: lambda _: None}, max_entries=1, ttl=300)
 # @st.cache(suppress_st_warning=True, hash_funcs={"AutoModelForTokenClassification": lambda _: None}, max_entries=1)
-@st.experimental_singleton
+@st.experimental_singleton(suppress_st_warning=True)
 def carrega_modelo(modelo):
     st.write('Cache miss: carrega_modelo(',modelo,')')
     modelo_treinado = AutoModelForTokenClassification.from_pretrained(modelo)
@@ -170,7 +170,7 @@ def carrega_modelo(modelo):
 # @st.cache(allow_output_mutation=True, ttl=3600)
 # @st.cache(suppress_st_warning=True, allow_output_mutation=True, max_entries=1, ttl=300)
 # @st.cache(suppress_st_warning=True, allow_output_mutation=True, max_entries=1)
-@st.experimental_singleton
+@st.experimental_singleton(suppress_st_warning=True)
 def carrega_tokenizer(modelo):
     st.write('Cache miss: carrega_tokenizer(',modelo,')')
     tokenizer_treinado = AutoTokenizer.from_pretrained(modelo)
